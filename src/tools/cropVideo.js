a178f39f49f78a8524ebac826fd93e798d9302ac
Leon564 <nleon564.nl@gmail.com>
2022-06-12 22:48:01 -0600

    refactor(): animated sticker conversion optimized

--- src/tools/cropVideo.js
+++ src/tools/cropVideo.js
@@ -2,7 +2,7 @@ const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
 const ffmpeg = require("fluent-ffmpeg");
 ffmpeg.setFfmpegPath(ffmpegPath);
 const { tmpdir } = require("os");
-const { readFileSync, writeFileSync, unlinkSync} = require("fs-extra");
+const { readFileSync, writeFileSync, unlinkSync } = require("fs-extra");
 
 const crop = async (file, fps) => {
   let isBuffer = Buffer.isBuffer(file);
@@ -10,30 +10,26 @@ const crop = async (file, fps) => {
     const tempFile = tmpdir() + "/" + Date.now() + ".video";
     writeFileSync(tempFile, file);
     file = tempFile;
-  }  
+  }
   const dir = `${tmpdir()}/${Date.now()}.gif`;
   const fm = new ffmpeg();
   return new Promise((resolve, reject) => {
     fm.input(file)
+      .keepDAR()      
       .outputOptions([
         "-vf",
-        `crop=w='min(min(iw\,ih)\,500)':h='min(min(iw\,ih)\,500)',scale=500:500,setsar=1,fps=${fps || 10}`,
-        "-loop",
-        "0",
-        "-preset",
-        "default",
-        "-an",
-        "-vsync",
-        "0",
-        "-fs",        
+        `crop=w='min(min(iw\,ih)\,500)':h='min(min(iw\,ih)\,500)',scale=500:500,setsar=1,fps=${
+          fps || 10
+        }`,
+        "-fs",
         "1MB",
         "-s",
         "512:512",
       ])
       .output(dir)
       .on("end", () => {
-        let gif = readFileSync(dir); 
-        unlinkSync(dir);        
+        let gif = readFileSync(dir);
+        unlinkSync(dir);
         if (isBuffer) unlinkSync(file);
         return resolve(gif);
       })
